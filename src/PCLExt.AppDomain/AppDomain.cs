39615d4d39bbede7e742569abff81328388c0c4e
Aragas <micvitalij@yandex.ru>
2016-04-30 02:46:39 +0300

    Removed non-important code

--- src/PCLExt.AppDomain/AppDomain.cs
+++ src/PCLExt.AppDomain/AppDomain.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reflection;
 
 namespace PCLExt.AppDomain
@@ -31,7 +30,7 @@ namespace PCLExt.AppDomain
             }
         }
 
-        internal static Exception NotImplementedInReferenceAssembly() => new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the PCLExt.AppDomain NuGet package from your main application project in order to reference the platform-specific implementation.");
+        internal static Exception NotImplementedInReferenceAssembly() => new NotImplementedException("This functionality is not implemented in the portable version of this assembly. You should reference the PCLExt.AppDomain NuGet package from your main application project in order to reference the platform-specific implementation.");
 
 
         /// <summary>
@@ -53,53 +52,5 @@ namespace PCLExt.AppDomain
         /// <param name="assemblyData"></param>
         /// <returns></returns>
         public static Assembly LoadAssembly(byte[] assemblyData) => Instance.LoadAssembly(assemblyData);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="className"></param>
-        /// <param name="assembly"></param>
-        /// <returns></returns>
-        public static Type GetTypeFromNameAndInterface<T>(string className, Assembly assembly)
-        {
-            foreach (var typeInfo in new List<TypeInfo>(assembly.DefinedTypes))
-                if (typeInfo.Name == className)
-                    foreach (var type in new List<Type>(typeInfo.ImplementedInterfaces))
-                        if (type == typeof (T))
-                            return typeInfo.AsType();
-
-            return null;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="className"></param>
-        /// <param name="assembly"></param>
-        /// <returns></returns>
-        public static Type GetTypeFromNameAndAbstract<T>(string className, Assembly assembly)
-        {
-            foreach (var typeInfo in new List<TypeInfo>(assembly.DefinedTypes))
-                if (typeInfo.Name == className)
-                    if (typeInfo.IsSubclassOf(typeof (T)))
-                        return typeInfo.AsType();
-
-            return null;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="className"></param>
-        /// <param name="assembly"></param>
-        /// <returns></returns>
-        public static Type GetTypeFromName(string className, Assembly assembly)
-        {
-            foreach (var typeInfo in new List<TypeInfo>(assembly.DefinedTypes))
-                if (typeInfo.Name == className)
-                    return typeInfo.AsType();
-
-            return null;
-        }
     }
 }
