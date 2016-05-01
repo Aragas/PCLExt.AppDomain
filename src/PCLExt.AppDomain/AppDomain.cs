8b4b6ef322b6064c6d0279333f435f1d27ef7dae
Aragas <micvitalij@yandex.ru>
2016-05-01 21:29:35 +0300

    Made it better

--- src/PCLExt.AppDomain/AppDomain.cs
+++ src/PCLExt.AppDomain/AppDomain.cs
@@ -8,29 +8,9 @@ namespace PCLExt.AppDomain
     /// </summary>
     public static class AppDomain
     {
-        static Lazy<IAppDomain> _instance = new Lazy<IAppDomain>(CreateInstance, System.Threading.LazyThreadSafetyMode.PublicationOnly);
-
-        static IAppDomain CreateInstance()
-        {
-#if COMMON
-            return new DesktopAppDomain();
-#endif
-
-            return null;
-        }
-
-        private static IAppDomain Instance
-        {
-            get
-            {
-                var ret = _instance.Value;
-                if (ret == null)
-                    throw NotImplementedInReferenceAssembly();
-                return ret;
-            }
-        }
-
-        internal static Exception NotImplementedInReferenceAssembly() => new NotImplementedException("This functionality is not implemented in the portable version of this assembly. You should reference the PCLExt.AppDomain NuGet package from your main application project in order to reference the platform-specific implementation.");
+        private static Exception NotImplementedInReferenceAssembly() => 
+            new NotImplementedException(@"This functionality is not implemented in the portable version of this assembly.
+You should reference the PCLExt.AppDomain NuGet package from your main application project in order to reference the platform-specific implementation.");
 
 
         /// <summary>
@@ -38,19 +18,40 @@ namespace PCLExt.AppDomain
         /// </summary>
         /// <param name="type"></param>
         /// <returns></returns>
-        public static Assembly GetAssembly(Type type) => Instance.GetAssembly(type);
+        public static Assembly GetAssembly(Type type)
+        {
+#if DESKTOP || ANDROID || __IOS__ || MAC
+            return Assembly.GetAssembly(type);
+#endif
+
+            throw NotImplementedInReferenceAssembly();
+        }
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public static Assembly[] GetAssemblies() => Instance.GetAssemblies();
+        public static Assembly[] GetAssemblies()
+        {
+#if DESKTOP || ANDROID || __IOS__ || MAC
+            return System.AppDomain.CurrentDomain.GetAssemblies();
+#endif
+
+            throw NotImplementedInReferenceAssembly();
+        }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="assemblyData"></param>
         /// <returns></returns>
-        public static Assembly LoadAssembly(byte[] assemblyData) => Instance.LoadAssembly(assemblyData);
+        public static Assembly LoadAssembly(byte[] assemblyData)
+        {
+#if DESKTOP || ANDROID || __IOS__ || MAC
+            return Assembly.Load(assemblyData);
+#endif
+
+            throw NotImplementedInReferenceAssembly();
+        }
     }
 }
