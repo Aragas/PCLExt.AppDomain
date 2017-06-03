b093c778a36d2cde61e99e5badfe7145513a40b4
Aragas <micvitalij@yandex.ru>
2017-06-03 13:27:25 +0300

    Removed GetAssembly, use GetTypeInfo().Assembly
    AssemblyResolve is now an event

---
+++ src/PCLExt.AppDomain.Abstractions/ResolveEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace PCLExt.AppDomain
+{
+    public class ResolveEventArgs : EventArgs
+    {
+        public string Name { get; }
+        public Assembly RequestingAssembly { get; }
+
+        public ResolveEventArgs(string name) { Name = name; }
+
+        public ResolveEventArgs(string name, Assembly requestingAssembly) { Name = name; RequestingAssembly = requestingAssembly; }
+    }
+}
--- src/PCLExt.AppDomain/AppDomain.cs
+++ src/PCLExt.AppDomain/AppDomain.cs
@@ -8,24 +8,10 @@ namespace PCLExt.AppDomain
     /// </summary>
     public static class AppDomain
     {
-        private static Exception NotImplementedInReferenceAssembly() => 
+        private static Exception NotImplementedInReferenceAssembly() =>
             new NotImplementedException(@"This functionality is not implemented in the portable version of this assembly.
 You should reference the PCLExt.AppDomain NuGet package from your main application project in order to reference the platform-specific implementation.");
 
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public static Assembly GetAssembly(Type type)
-        {
-#if DESKTOP || ANDROID || __IOS__ || MAC
-            return Assembly.GetAssembly(type);
-#endif
-
-            throw NotImplementedInReferenceAssembly();
-        }
 
         /// <summary>
         /// 
@@ -53,5 +39,37 @@ You should reference the PCLExt.AppDomain NuGet package from your main applicati
 
             throw NotImplementedInReferenceAssembly();
         }
+
+#if DESKTOP || ANDROID || __IOS__ || MAC
+        public static event ResolveEventHandler AssemblyResolve
+        {
+            add { System.AppDomain.CurrentDomain.AssemblyResolve += DelegateUtility.Cast<System.ResolveEventHandler>(value); }
+            remove { System.AppDomain.CurrentDomain.AssemblyResolve -= DelegateUtility.Cast<System.ResolveEventHandler>(value); }
+        }
+        private static class DelegateUtility
+        {
+            public static T Cast<T>(Delegate source) where T : class => Cast(source, typeof(T)) as T;
+            public static Delegate Cast(Delegate source, Type type)
+            {
+                if (source == null)
+                    return null;
+                var delegates = source.GetInvocationList();
+                if (delegates.Length == 1)
+                    return Delegate.CreateDelegate(type, delegates[0].Target, delegates[0].Method);
+
+                var delegatesDest = new Delegate[delegates.Length];
+                for (var nDelegate = 0; nDelegate < delegates.Length; nDelegate++)
+                    delegatesDest[nDelegate] = Delegate.CreateDelegate(type, delegates[nDelegate].Target, delegates[nDelegate].Method);
+
+                return Delegate.Combine(delegatesDest);
+            }
+        }
+#else
+        public static event ResolveEventHandler AssemblyResolve
+        {
+            add => throw NotImplementedInReferenceAssembly();
+            remove => throw NotImplementedInReferenceAssembly();
+        }
+#endif
     }
 }
